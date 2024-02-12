3240de15ff0aac6715f5c35908d03c31a40baa83
Noran Sherif <>
2024-02-12 13:34:22 +0200

    use thunk middleWare

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,12 +2,17 @@ const accountInitialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = accountInitialState, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: action.payload + state.balance };
+      return {
+        ...state,
+        balance: action.payload + state.balance,
+        isLoading: false,
+      };
 
     case "account/withdraw":
       return {
@@ -32,13 +37,30 @@ export default function accountReducer(state = accountInitialState, action) {
         balance: state.balance - state.loan,
       };
 
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
     default:
       return state;
   }
 }
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?${amount}=10&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 
 export function withdraw(amount) {
--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
@@ -6,6 +8,9 @@ const rootReducers = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducers);
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
